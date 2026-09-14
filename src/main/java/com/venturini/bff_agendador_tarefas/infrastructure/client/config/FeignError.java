78f460b8f8328a6d35c31d9660181e3fb220aa3f
diegoventurini <diegoventurini27@gmail.com>
2026-09-14 09:23:39 -0300

    Modificado o tipo de error no FeignError 403

--- src/main/java/com/venturini/bff_agendador_tarefas/infrastructure/client/config/FeignError.java
+++ src/main/java/com/venturini/bff_agendador_tarefas/infrastructure/client/config/FeignError.java
@@ -2,6 +2,7 @@ package com.venturini.bff_agendador_tarefas.infrastructure.client.config;
 
 import com.venturini.bff_agendador_tarefas.infrastructure.exceptions.BusinessException;
 import com.venturini.bff_agendador_tarefas.infrastructure.exceptions.ConflictException;
+import com.venturini.bff_agendador_tarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.venturini.bff_agendador_tarefas.infrastructure.exceptions.UnauthorizedException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
@@ -16,7 +17,7 @@ public class FeignError implements ErrorDecoder {
             case 409:
                 return new ConflictException("Error: atributo já existente.");
             case 403:
-                return new ReflectiveOperationException("Error: atributo não encontrado.");
+                return new ResourceNotFoundException("Error: atributo não encontrado.");
             case 401:
                 return new UnauthorizedException("Error: usuário não autorizado.");
             default:
